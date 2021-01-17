fde2dc42d9737d4b181ed737d63acde70cd441f6
Bruno Garcia <b@aduros.com>
2021-01-17 17:22:22 -0300

    Implement csv and json.

--- cli.js
+++ cli.js
@@ -1,31 +1,46 @@
 #!/usr/bin/env node
 
+"use strict";
+
 const commander = require("commander");
-const package = require("./package.json");
+const pkg = require("./package.json");
 
 function collect (value, previous) {
+    if (previous == null) {
+        previous = [];
+    }
     return previous.concat([value]);
 }
 
 const program = new commander.Command()
-    .description(package.description)
-    .version(package.version)
-    .arguments("<code...>")
-    .option("-x, --explain")
-    .option("-b, --before <code>", "Run code before", collect, [])
-    .option("-a, --after <code>", "Run code after", collect, [])
-    .option("-d, --delimiter <delimiter>", "Set the delimiter", "\\s+")
-    // .option("--json")
-    // .option("--csv")
-    .action(async (code, options, command) => {
+    .description(pkg.description)
+    .version(pkg.version)
+    .arguments("<script...>")
+    .option("-x, --explain", "Print generated program instead of running it")
+    .option("-b, --before <script>", "Run script before parsing", collect)
+    .option("-a, --after <script>", "Run script after parsing", collect)
+    .option("-d, --delimiter <delimiter>", "The delimiter for text parsing", "\\s+")
+    .option("--csv", "Parse input as CSV")
+    .option("--csv-header", "Parse input as CSV with a column header")
+    .option("--json <filter>", "Parse input as JSON")
+    .action(async (script, options, command) => {
         const pjs = require(".");
         var generated;
         try {
+            let mode = null;
+            if (options.csv || options.csvHeader) {
+                mode = "csv";
+            } else if (options.json) {
+                mode = "json";
+            }
             generated = pjs.generate({
-                beforeJs: options.before.join("\n"),
-                lineJs: code.join("\n"),
-                afterJs: options.after.join("\n"),
-                delimiter: (options.delimiter != null) ? new RegExp(options.delimiter) : null,
+                mode,
+                beforeJs: options.before ? options.before.join("\n") : null,
+                lineJs: script.join("\n"),
+                afterJs: options.after ? options.after.join("\n") : null,
+                textDelimiter: (options.delimiter != null) ? new RegExp(options.delimiter) : null,
+                csvHeader: options.csvHeader,
+                jsonFilter: options.json,
             });
         } catch (error) {
             // console.error("Parse error: "+error.message);
